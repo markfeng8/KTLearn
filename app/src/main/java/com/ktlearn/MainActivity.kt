b26fb8a345fbb675096c8ebde426e42ca7eb79f1
1246114057@qq.com <1246114057@qq.com>
2021-07-24 13:40:15 +0800

    Kotlin学习

--- app/src/main/java/com/ktlearn/MainActivity.kt
+++ app/src/main/java/com/ktlearn/MainActivity.kt
@@ -1,31 +1,24 @@
 package com.ktlearn
 
+import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    companion object {
-        private const val TAG = "MainActivity"
-    }
-
-    //延迟申明var变量
-    private lateinit var tvname: TextView
-    private val msgList = listOf("123", "2", "3", "4")
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        tvname.setOnClickListener {
-            Toast.makeText(this, "这是一个", Toast.LENGTH_LONG).show()
-            for (str in msgList) {
-                Log.i(TAG, str)
-            }
+        val tvName: TextView = findViewById(R.id.tv_name)
+
+        tvName.setOnClickListener {
+            toast(this,"tishi").show()
         }
 
     }
+
+    fun toast(context: Context, msg: String):Toast = Toast.makeText(context, msg, Toast.LENGTH_LONG)
 }
